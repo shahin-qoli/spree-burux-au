4943145994ed1f527143bf0535549acee8b22726
shahin <shaahin.qoli@gmail.com>
2022-06-29 03:38:21 -0700

    api added3

--- lib/spree/user_controller.rb
+++ lib/spree/user_controller.rb
@@ -1,7 +1,7 @@
 require 'httparty'
 require 'json'
 
-class Spree::UsersController < StoreController
+class Spree::UsersController < ApplicationController
   def disable_otp
     current_user.otp_required_for_login = false
     current_user.save!
