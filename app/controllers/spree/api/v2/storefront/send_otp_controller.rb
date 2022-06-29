b9e0dd180d8bee2810fbb3c99735aaeef2709dbd
shahin <shaahin.qoli@gmail.com>
2022-06-29 00:54:41 -0700

    api added

--- app/controllers/spree/api/v2/storefront/send_otp_controller.rb
+++ app/controllers/spree/api/v2/storefront/send_otp_controller.rb
@@ -3,7 +3,7 @@ module Spree
     module V2
       module Storefront
         class SendOtpController < ::Spree::Api::V2::BaseController
-          #include Spree::Core::ControllerHelpers::Store
+          include Spree::Core::ControllerHelpers::Store
 
           def create
             user = Spree.user_class.find_by(mobile_number: params[:mobile_number])
