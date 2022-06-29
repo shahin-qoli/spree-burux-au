a96c9adb78fc2eae0dbf7c3889c63ef0648d4a5e
shahin <shaahin.qoli@gmail.com>
2022-06-29 02:29:34 -0700

    api added

--- app/controllers/spree/api/v2/storefront/send_otp_controller.rb
+++ app/controllers/spree/api/v2/storefront/send_otp_controller.rb
@@ -12,7 +12,7 @@ module Spree
               user.otp_secret = Spree::User.generate_otp_secret
               username = user.email
               otp_code = user.current_otp
-    
+              @mobile = user.mobile_number
               username = user.email
               request_url  = "https://sms.magfa.com/api/http/sms/v1?service=enqueue&username=paydar_75116&password=V7WXGbTUEZMZVoJT&domain=magfa&from=300075116&to=#{@mobile}&text=code for login to BuruxShop is #{otp_code}"
               HTTParty.get(request_url)              
