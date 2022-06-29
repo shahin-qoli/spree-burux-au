10760e915ab6a435f1d6a0c31d35f0e1a09e0ea7
shahin <shaahin.qoli@gmail.com>
2022-06-29 00:39:47 -0700

    api added

--- config/routes.rb
+++ config/routes.rb
@@ -55,7 +55,7 @@ Spree::Core::Engine.add_routes do
     namespace :v2 do
       namespace :storefront do
         resource :account, controller: :account, only: %i[show create update]
-        resources :account_confirmations, only: %i[show]
+        resources :account_confirmations22, only: %i[show]
         resources :passwords, controller: :passwords, only: %i[create update]
         resources :send_otp, only: %i[create]
       end
