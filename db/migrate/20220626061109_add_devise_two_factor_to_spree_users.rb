51f29d1706d596c3ad131fe012af0102ccfa063a
shahin <shaahin.qoli@gmail.com>
2022-07-07 04:06:01 -0700

    migration changed

--- db/migrate/20220626061109_add_devise_two_factor_to_spree_users.rb
+++ db/migrate/20220626061109_add_devise_two_factor_to_spree_users.rb
@@ -1,11 +1,12 @@
-class AddDeviseTwoFactorToSpreeUsers < ActiveRecord::Migration[7.0]
+class AddDeviseTwoFactorToSpreeUsers < ActiveRecord::Migration[6.0]
   def change
     add_column :spree_users, :encrypted_otp_secret, :string
     add_column :spree_users, :encrypted_otp_secret_iv, :string
     add_column :spree_users, :encrypted_otp_secret_salt, :string
     add_column :spree_users, :consumed_timestep, :integer
     add_column :spree_users, :otp_required_for_login, :boolean
-    add_index :spree_users, :mobile_number, unique: true, default: ""
+    add_column :spree_users, :mobile_number, :string
+    add_index :spree_users, :mobile_number, unique: true
     remove_index :spree_users, :email
     add_index :spree_users, :email
   end
